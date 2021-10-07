33c806f8d5766fc2af87a6aaa32489f796ab0846
unknown <uvanov977777@gmail.com>
2021-10-07 19:59:12 +0300

    int commit

---
+++ scripts/fade-slider.js
@@ -0,0 +1,25 @@
+(function(){
+
+const slides = document.querySelectorAll('.fade-slider__item')  
+const activeClass = 'fade-slider__item-visble'
+let index = 0;
+
+
+
+setInterval(()=>{
+
+slides[index].classList.remove(activeClass )
+
+index = index + 1;
+
+if(index + 1 > slides.length){
+   index = 0;
+}
+
+slides[index].classList.add(activeClass)
+
+
+}, 5000)
+
+
+}())
